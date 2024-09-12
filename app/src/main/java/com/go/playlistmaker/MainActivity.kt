b1b9e524f317838f73ec25ccb603a1f0cab02ca5
Brekhov Nikita <brekhov.nikita@gmail.com>
2024-09-12 16:04:31 +0500

    activities and layouts have been added

--- app/src/main/java/com/go/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/go/playlistmaker/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.go.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonLibrary = findViewById<Button>(R.id.library)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+
+        buttonSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        buttonLibrary.setOnClickListener {
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
