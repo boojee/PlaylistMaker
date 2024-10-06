bb4e14bb784d7f3ad38dc662ae29bece29ec2d23
Brekhov Nikita <brekhov.nikita@gmail.com>
2024-10-06 23:58:55 +0500

    add RecyclerView for search

---
+++ app/src/main/java/com/go/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,29 @@
+package com.go.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val albumIcon: ImageView = itemView.findViewById(R.id.album_cover)
+    private val trackName: TextView = itemView.findViewById(R.id.track_name)
+    private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = track.artistName
+        trackTime.text = track.trackTime
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.ic_track_placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(10))
+            .into(albumIcon)
+    }
+}
